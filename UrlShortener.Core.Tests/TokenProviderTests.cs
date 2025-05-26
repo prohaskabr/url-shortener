2025298dded1577fd2277022ee863b939f35be13
Thiago Prohaska <thiago.prohaska@gmail.com>
2025-05-26 14:48:02 +0200

    Add lock to TokenProvider

---
+++ UrlShortener.Core.Tests/TokenProviderTests.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using System.Collections.Concurrent;
+
+namespace UrlShortener.Core.Tests;
+
+public class TokenProviderTests
+{
+    [Fact]
+    public void Should_get_token_from_start()
+    {
+        var tokenProvider = new TokenProvider();
+        tokenProvider.AssignRange(10, 1000);
+
+        tokenProvider.GetToken().Should().Be(10);
+    }
+
+    [Fact]
+    public void Should_increment_token_on_get()
+    {
+        var tokenProvider = new TokenProvider();
+        tokenProvider.AssignRange(10, 1000);
+        tokenProvider.GetToken();
+
+        tokenProvider.GetToken().Should().Be(11);
+    }
+
+    [Fact]
+    public void Should_not_return_same_token_twice()
+    {
+        const int Start = 1;
+        const int End = 1000;
+
+        var tokenProvider = new TokenProvider();
+        ConcurrentBag<long> tokens = new ConcurrentBag<long>();
+        tokenProvider.AssignRange(Start, End);
+
+
+        Parallel.ForEach(Enumerable.Range(Start, End), _ =>
+        {
+            tokens.Add(tokenProvider.GetToken());
+        });
+
+        tokens.Should().OnlyHaveUniqueItems();
+    }
+}
--- UrlShortener.Core/TokenProvider.cs
+++ UrlShortener.Core/TokenProvider.cs
@@ -2,19 +2,26 @@
 
 public class TokenProvider
 {
+    private readonly object _lock = new();
+    private long _token = 0;
     TokenRange _tokenRange;
+
     public void AssignRange(TokenRange tokenRange)
     {
         _tokenRange = tokenRange;
+        _token = _tokenRange.Start;
     }
 
     public void AssignRange(long start, long end)
     {
-        _tokenRange = new TokenRange(start, end);
+        AssignRange(new TokenRange(start, end));
     }
 
     public long GetToken()
     {
-        return _tokenRange.Start;
+        lock (_lock)
+        {
+            return _token++;
+        }
     }
 }
